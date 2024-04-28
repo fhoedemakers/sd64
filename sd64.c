ef4dbf1fd27ae0caccd9889e77fa90f9e18091f5
Frank Hoedemakers <frank.hoedemakers@gmail.com>
2024-04-28 15:35:44 +0200

    Update sd64.c to list files and directories in the root folder of the SD card

--- sd64.c
+++ sd64.c
@@ -2,11 +2,23 @@
 
 // Read the contents of the SD card on an Everdrive 64
 // Tested on an Everdrive 64 X7
-// This code will list all files and directories in the /filesystem folder of the SD card
-// Create a folder named "filesystem" on the root of the SD card and put some files in it.
+// This code will list all files and directories in the / folder of the SD card
+// This code will not work on an emulator, it must be run on real hardware with an Everdrive attached
 int main() {
-    debug_init(DEBUG_FEATURE_LOG_ISVIEWER);
+    debug_init(DEBUG_FEATURE_LOG_ISVIEWER | DEBUG_FEATURE_LOG_USB);
     console_init();
+    controller_init();
+    printf("SD64 - List files and directories on the SD card\n\n");
+    printf("This code will list all files and directories in the root folder of the SD card\n");
+    printf("This code will not work on an emulator, it must be run on real hardware with an Everdrive attached\n\n");
+    printf("Press A to continue\n");
+    while(1) {
+        controller_scan();
+        struct controller_data keys = get_keys_down();
+        if (keys.c[0].A) {
+            break;
+        }
+    }
 	/* This will initialize the SD filesystem using 'sd:/' to identify it */
 	if (!debug_init_sdfs("sd:/", -1)) {
 		// debugf not necessary, printf will show the message on the emulator screen and the console.
